e88745673433a8c36bc756fdf2aa37f3512e86e1
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-04-18 14:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here
 let table = document.querySelector('table');
 let newRow = document.createElement('tr');
-let prices = document.querySelectorAll('[data-ns-test="prices"]');
+let prices = document.querySelectorAll('[data-ns-test="price"]');
 let sum = 0;
 prices.forEach((item) => {sum+=(parseInt(item.textContent))});
 let total = document.createElement('tr');
