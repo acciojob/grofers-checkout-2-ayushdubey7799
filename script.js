eb2180b083004d02c6eb94408cc9c7a4e7a3f132
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-04-18 14:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let table = document.querySelector('table');
+let newRow = document.createElement('tr');
+let prices = document.querySelectorAll('[data-ns-test="prices"]');
+let sum = 0;
+prices.forEach((item) => {sum+=(parseInt(item.textContent))});
+let total = document.createElement('tr');
+total.setAttribute("data-ns-test","grandTotal")
+total.textContent = sum;
+newRow.appendChild(total);
+table.appendChild(newRow);
